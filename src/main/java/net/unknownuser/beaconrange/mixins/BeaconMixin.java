3d9998e83aca4dcc7b83ae7004f54969d8275077
James Anderson <joji@ando.ws>
2026-01-12 22:27:06 +0000

    Reduce the applyPlayerEffects mixin to only change the range double variable

--- src/main/java/net/unknownuser/beaconrange/mixins/BeaconMixin.java
+++ src/main/java/net/unknownuser/beaconrange/mixins/BeaconMixin.java
@@ -1,11 +1,9 @@
 package net.unknownuser.beaconrange.mixins;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.*;
-import net.minecraft.entity.effect.*;
-import net.minecraft.entity.player.*;
 import net.minecraft.registry.*;
-import net.minecraft.registry.entry.*;
 import net.minecraft.registry.tag.*;
 import net.minecraft.util.*;
 import net.minecraft.util.math.*;
@@ -15,69 +13,16 @@ import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.*;
 
-import java.util.*;
-
 @Mixin(BeaconBlockEntity.class)
 public class BeaconMixin {
 	private BeaconMixin() {}
 	
 	@Unique
 	private static double baseBlockFactor = 1;
-	
-	@Inject(at = @At("HEAD"), method = "applyPlayerEffects", cancellable = true)
-	private static void applyPlayerEffects(
-		World world,
-		BlockPos pos,
-		int beaconLevel,
-		RegistryEntry<StatusEffect> primaryEffect,
-		RegistryEntry<StatusEffect> secondaryEffect,
-		CallbackInfo ci
-	) {
-		// This method is nearly entirely copied from the default implementation, except the range declaration line
-		
-		// replace method entirely
-		ci.cancel();
-		
-		if (!world.isClient() && primaryEffect != null) {
-			double range           = (beaconLevel * Config.rangePerLevel() * baseBlockFactor) + Config.baseOffset(); // CHANGES MADE HERE
-			int    effectAmplifier = 0;
-			
-			if (beaconLevel >= 4 && Objects.equals(primaryEffect, secondaryEffect)) {
-				effectAmplifier = 1;
-			}
-			
-			int                    effectDuration = (9 + beaconLevel * 2) * 20;
-			Box                    rangeBox       = new Box(pos).expand(range).stretch(0.0, world.getHeight(), 0.0);
-			List<PlayerEntity>     players        = world.getNonSpectatingEntities(PlayerEntity.class, rangeBox);
-			Iterator<PlayerEntity> playerIterator = players.iterator();
-			
-			PlayerEntity playerEntity;
-			while (playerIterator.hasNext()) {
-				playerEntity = playerIterator.next();
-				playerEntity.addStatusEffect(new StatusEffectInstance(
-					primaryEffect,
-					effectDuration,
-					effectAmplifier,
-					true,
-					true
-				));
-			}
-			
-			if (beaconLevel >= 4 && !Objects.equals(primaryEffect, secondaryEffect) && secondaryEffect != null) {
-				playerIterator = players.iterator();
-				
-				while (playerIterator.hasNext()) {
-					playerEntity = playerIterator.next();
-					playerEntity.addStatusEffect(new StatusEffectInstance(
-						secondaryEffect,
-						effectDuration,
-						0,
-						true,
-						true
-					));
-				}
-			}
-		}
+
+	@ModifyVariable(method = "applyPlayerEffects", at = @At("STORE"))
+	private static double applyNewBeaconRange(double range, @Local(argsOnly = true) int beaconLevel) {
+		return (beaconLevel * Config.rangePerLevel() * baseBlockFactor) + Config.baseOffset();
 	}
 	
 	@Inject(at = @At("HEAD"), method = "updateLevel", cancellable = true)
