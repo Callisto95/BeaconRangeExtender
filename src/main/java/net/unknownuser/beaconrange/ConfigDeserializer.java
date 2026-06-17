7f37c77d84a6a3b9655948209cbb33b1b9055e57
Callisto95 <luluklzde@gmail.com>
2026-06-17 09:49:13 +0200

    write a proper TypeAdapter

--- src/main/java/net/unknownuser/beaconrange/ConfigDeserializer.java
+++ src/main/java/net/unknownuser/beaconrange/ConfigDeserializer.java
@@ -1,63 +1,74 @@
 package net.unknownuser.beaconrange;
 
 import com.google.gson.*;
-import net.minecraft.util.*;
+import com.google.gson.stream.*;
+import net.minecraft.core.registries.*;
+import net.minecraft.resources.*;
+import net.minecraft.world.level.block.*;
 
-import java.lang.reflect.*;
+import java.io.*;
 import java.util.*;
 
-// if this does not exist, Gson uses type defaults for each variable
-// I hate this
-// just use Jackson.
-
-// This is technically not needed, but I hate whatever Gson is doing too much to just keep it
-public class ConfigDeserializer implements JsonDeserializer<Config> {
+public class ConfigDeserializer extends TypeAdapter<Config> {
 	@Override
-	public Config deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext)
-		throws JsonParseException {
-		JsonObject object  = jsonElement.getAsJsonObject();
-		FieldGetter getter = new FieldGetter(object);
+	public void write(JsonWriter writer, Config value) throws IOException {
+		if (value == null) {
+			writer.nullValue();
+			return;
+		}
 		
-		// NOTE:
-		// the names MUST equal Config.[attribute], otherwise there will be errors!
-		int rangePerLevel = getter.getInt("rangePerLevel", Config.Defaults.RANGE_PER_LEVEL_MULTIPLIER);
-		int baseOffset    = getter.getInt("baseRange", Config.Defaults.MINIMUM_RANGE);
+		writer.setFormattingStyle(FormattingStyle.PRETTY);
+		writer.beginObject()
+			.name("rangePerLevel")
+			.value(value.rangePerLevel)
+			.name("baseRange")
+			.value(value.baseRange)
+			.name("rangeMultipliers")
+			.beginObject();
 		
-		Map<String, JsonElement> rawMultipliers = getter.getMap("rangeMultipliers");
-		Map<Identifier, Double>  multipliers    = new HashMap<>();
+		for (Map.Entry<Block, Double> entry : value.rangeMultipliers.entrySet()) {
+			Block  block      = entry.getKey();
+			Double multiplier = entry.getValue();
+			writer.name(BuiltInRegistries.BLOCK.getKey(block).toString()).value(multiplier);
+		}
 		
-		for (Map.Entry<String, JsonElement> element : rawMultipliers.entrySet()) {
-			try {
-				Identifier identifier = Identifier.splitOn(element.getKey(), ':');
-				double     multiplier = element.getValue().getAsDouble();
-				multipliers.put(identifier, multiplier);
-			} catch (InvalidIdentifierException exc) {
-				BeaconRange.LOGGER.error("Invalid block identifier in config: '{}'", element.getKey());
-				throw exc;
-			} catch (UnsupportedOperationException exc) {
-				BeaconRange.LOGGER.error(
-					"Identifier '{}' has invalid value '{}'",
-					element.getKey(),
-					element.getValue()
-				);
-				throw exc;
+		writer.endObject().endObject();
+	}
+	
+	@Override
+	public Config read(JsonReader reader) throws IOException {
+		int                rangePerLevel    = Config.Defaults.RANGE_PER_LEVEL_MULTIPLIER;
+		int                baseRange        = Config.Defaults.MINIMUM_RANGE;
+		Map<Block, Double> rangeMultipliers = new HashMap<>();
+		
+		reader.beginObject();
+		
+		while (reader.hasNext()) {
+			String fieldName = reader.nextName();
+			
+			switch (fieldName) {
+				case "rangePerLevel" -> rangePerLevel = reader.nextInt();
+				case "baseRange" -> baseRange = reader.nextInt();
+				case "rangeMultipliers" -> {
+					reader.beginObject();
+					
+					while (!reader.peek().equals(JsonToken.END_OBJECT)) {
+						String blockID    = reader.nextName();
+						double multiplier = reader.nextDouble();
+						
+						Block block = BuiltInRegistries.BLOCK.get(Identifier.parse(blockID)).orElseThrow().value();
+						
+						rangeMultipliers.put(block, multiplier);
+					}
+					
+					reader.endObject();
+				}
+				default -> BeaconRange.LOGGER.warn("Unknown config key: {}", fieldName);
 			}
 		}
 		
-		/*
-		This shouldn't be done -> player might have disabled it
+		reader.endObject();
 		
-		if (multipliers.isEmpty()) {
-			multipliers = Config.Defaults.RANGE_EXTENDER_MULTIPLIER;
-		}
-		 */
-		
-		Config config = new Config(rangePerLevel, baseOffset, multipliers);
-		
-		if (getter.hasError()) {
-			config.enableError();
-		}
-		
-		return config;
+		return new Config(rangePerLevel, baseRange, rangeMultipliers);
 	}
 }
